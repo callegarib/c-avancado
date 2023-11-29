acb19310e8da0b4e327bc408a4f47ab9ab7631fe
Beatriz Callegari <callegari.bia@gmail.com>
2023-11-28 21:36:11 -0300

    Inserção de Pastas, classes e soluções de projeto

---
+++ ProjetoEmTresCamadas/ProjetoEmTresCamadas.Pizzaria.RegraDeNegocio/Pizza.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEmTresCamadas.Pizzaria.RegraDeNegocio
+{
+    public enum TamanhoDePizza
+    {
+        Pequena = 0,
+        Media = 1,
+        Grande = 2
+    }
+    public class Pizza
+    {
+        public string Sabor { get; set; }
+
+        public TamanhoDePizza TamanhoDePizza { get; set; }
+        public string Descricao { get; set; }
+
+        public Pizza()
+        {
+
+        }
+
+        public Pizza CriarPizza
+            (string sabor, TamanhoDePizza tamanhoDePizza, string descricao = "")
+        {
+            Sabor = sabor;
+            TamanhoDePizza = tamanhoDePizza;
+            if (string.IsNullOrEmpty(descricao))
+            {
+                Descricao = descricao;
+            }
+            return this;
+        }
+    }
+}
